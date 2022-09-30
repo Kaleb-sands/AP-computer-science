5de2c14ed3f9effa0d784f6e343f54f71d1635ec
Kaleb-sands <Kaleb.sands@gmail.com>
2022-09-30 10:50:00 -0400

    reent project

---
+++ Exercises/ch02/Facemaker/FaceMaker.java
@@ -0,0 +1,11 @@
+public class FaceMaker
+{
+    public static void main(String[] args){
+        System.out.println("   xxxxx");
+        System.out.println("  x     x");
+        System.out.println("((  o o  ))");
+        System.out.println(" |   V   |");
+        System.out.println(" |  ===  |");
+        System.out.println("  ------- ");
+    }
+}
---
+++ Exercises/ch02/First project/MyBio.java
@@ -0,0 +1,14 @@
+public class MyBio
+{
+    public static void main(String[] args) {
+        System.out.println("*/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/*");
+        System.out.println(" |            !Kaleb Sands!            |");
+        System.out.println("*---------------------------------------*");
+        System.out.println(" |-- I like to rock climb              |");
+        System.out.println(" |-- I am a sophomore in highschool    |");
+        System.out.println(" |-- I like to play video games        |");
+        System.out.println(" |-- I am working on a gokart          |");
+        System.out.println(" |-- My favorite food is steak or pasta|");
+        System.out.println("*---------------------------------------*");
+    }
+}
