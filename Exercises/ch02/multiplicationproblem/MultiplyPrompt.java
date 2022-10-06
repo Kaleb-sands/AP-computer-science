d0ba26e61bb9683234932443b31ddf7896c77f6f
Kaleb-sands <Kaleb.sands@gmail.com>
2022-10-06 08:48:42 -0400

    Multiply Prompt project

---
+++ Exercises/ch02/multiplicationproblem/MultiplyPrompt.java
@@ -0,0 +1,24 @@
+//this project multiplies a integer by 2
+import java.util.Scanner;
+
+public class MultiplyPrompt
+{
+    public static void main(String[]args)
+    {
+        Scanner kb  = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        String firstNumber = kb.nextLine();
+
+        System.out.println("Enter second number;");
+        String secondNumber = kb.nextLine();
+
+        int x = Integer.parseInt(firstNumber);
+        int y = Integer.parseInt(secondNumber);
+        int z = x * y;
+
+        System.out.println(x + " * " + y + " = " + z);
+        kb.close();
+    }
+        
+}
