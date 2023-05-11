8226e97bb88c2a35dde47f9cb14ec7dd859ae831
Kaleb-sands <Kaleb.sands@gmail.com>
2023-05-11 12:42:00 -0400

    push

--- Exercises/0.5/TicTacToe/TicTacToe.java
+++ Exercises/0.5/TicTacToe/TicTacToe.java
@@ -1,24 +1,97 @@
 public class TicTacToe{
-    private String board[][] = {
-        {"","",""},
-        {"","",""},
-        {"","","",},
-    };
-    public void print(){
+    private static String board[][] = new String[3][3];
+    public TicTacToe(){
+        for(int r = 0; r < board.length; r++){
+            for(int c = 0; c < board[0].length; c++){
+                board[r][c] = "";
+            }
+        }
+    }
+
+    public static void initializeBoard(){
+        for(int r = 0; r < board.length; r++){
+            for(int c = 0; c < board[0].length; c++){
+                board[r][c] = "";
+            }
+        }
+    }
+
+    public static boolean isBoardFull(){
+        for(int r = 0; r < board.length; r++){
+            for(int c = 0; c < board[0].length; c++){
+                if(board[r][c].equals("")){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isEmpty(int r, int c){
+        if(board[r][c].equals("")){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean placeMarker(int r, int c, String marker){
+        if(isEmpty(r,c)){
+            board[r][c] = marker;
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkDiagsForWin(String player){
+        if(board[0][0].equals(player) && board[1][1].equals(player) && board[2][2].equals(player)){
+            return true;
+        }else if(board[0][2].equals(player) && board[1][1].equals(player) && board[2][0].equals(player)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private static boolean checkRowsForWin(String player){
+        for(int i = 0; i < board.length; i++){
+            if(board[i][0].equals(player) && board[i][1].equals(player) && board[i][2].equals(player)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkColsForWin(String player){
+        for(int i = 0; i < board.length; i++){
+            if(board[0][i].equals(player) && board[1][i].equals(player) && board[2][i].equals(player)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkForWin(String player){
+        if(checkDiagsForWin(player) || checkRowsForWin(player) || checkColsForWin(player)){
+            return true;
+        }
+        return false;
+    }
+    
+    public static void displayboard(){
         for(int row = 0; row < board.length; row++){
-            System.out.println("-----");
+            System.out.println("\n-------------");
             System.out.print("|");
             for(int col = 0; col < board[0].length; col++){
                 if(board[row][col].equalsIgnoreCase("x")){
-                    System.out.print("x");
+                    System.out.print(" x ");
                 }else if(board[row][col].equalsIgnoreCase("o")){
-                    System.out.print("o");
+                    System.out.print(" o ");
                 }else{
-                    System.out.print(" ");
+                    System.out.print("   ");
                 }
                 System.out.print("|");
             }
-            System.out.println("-----");
         }
+        System.out.println("\n-------------");
     }
 }
